09114d61d8856779baff6301c901a2fc9a7da9c8
sergey05 <sergey_psw@tut.by>
2013-08-18 21:17:37 +0300

    add some other files

--- MiniSocialNetwork.Web/Global.asax.cs
+++ MiniSocialNetwork.Web/Global.asax.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DependencyInjection;
+using EFContextLayer;
 
 namespace MiniSocialNetwork.Web
 {
@@ -16,8 +14,9 @@ namespace MiniSocialNetwork.Web
     {
         protected void Application_Start()
         {
+            DependencyResolver.SetResolver(DependencyInjector.GetResolver());
+            MiniSocialNetworkContext.SetInitializer();
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
